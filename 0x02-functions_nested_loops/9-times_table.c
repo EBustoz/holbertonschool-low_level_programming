8bd350f76b7948993fd7864a3926a57875547359
Ebustoz <ebustozm@gmail.com>
2022-02-08 18:56:52 +0000

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main : Call a function that prints he 9 times table, starting with 0.
+
+ *Return: Always 0 (succes)
+ */
+
+int main (void)
+{
+	times_table();
+  return (0);
+}
