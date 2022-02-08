16fa2e52cd924abe85e1dbcdc08084d770e6d2e4
Ebustoz <ebustozm@gmail.com>
2022-02-08 07:13:29 +0000

    12. Software is eating the World - last review

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,32 +9,23 @@ int main(void)
 {
 	int tens;
 	int ones;
-	int t;
-	int o;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
+
+	for (tens = 0; tens <= 98; tens++) 
 	{
-		for (ones = '0'; ones <= '9'; ones++)
+		for (ones = tens+1; ones <= 99; ones++)
 		{
-		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
-			{
-				for (o = ones + 1; o <= '9'; o++)
-				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(t);
-					putchar(o);
+			putchar((tens/10) + '0');
+			putchar((tens%10) + '0');
+			putchar(' ');
+			putchar((ones/10) + '0');
+			putchar((ones%10) + '0');
 
-					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				o = '0';
-			}
+			if (!(tens == 98 && ones == 99 ))					  
+			{
+				putchar(',');
+				putchar(' ');
+			}			
 		}
 	}
 	putchar('\n');
