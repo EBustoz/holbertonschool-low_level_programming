af6b01f3bbaa6a4db6c36b196e252b99bab89567
Ebustoz <ebustozm@gmail.com>
2022-02-08 18:47:01 +0000

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main : Call a function that prints the last digit of a number
+
+ *Return: Always 0 (succes)
+ */
+
+int main (void)
+{
+	jack_bauer();
+  return (0);
+}
