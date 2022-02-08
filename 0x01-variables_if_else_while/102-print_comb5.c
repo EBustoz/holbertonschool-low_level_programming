f5c93d30215bc8b4a8262a8318b1c2739be54a5d
Ebustoz <ebustozm@gmail.com>
2022-02-08 07:29:00 +0000

    12. Software is eating the World - last review5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int tens;
 	int ones;
-	
+
 	for (tens = 0; tens <= 98; tens++)
 	{
 		for (ones = tens + 1; ones <= 99 ; ones++)
@@ -19,7 +19,7 @@ int main(void)
 			putchar(' ');
 			putchar((ones / 10) + '0');
 			putchar((ones % 10) + '0');
-			if (!(tens == 98 && ones == 99 ))
+			if (!(tens == 98 && ones == 99))
 			{
 				putchar(',');
 				putchar(' ');
