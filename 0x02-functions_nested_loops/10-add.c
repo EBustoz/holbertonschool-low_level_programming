c9a2a0378c6b31f9d58546e1e780a629a302adf6
Ebustoz <ebustozm@gmail.com>
2022-02-08 19:03:08 +0000

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main : Call a function that prints he 9 times table, starting with 0.
+
+ *Return: Always 0 (succes)
+ */
+
+int main (void)
+{
+	int n;
+
+  n = add(89, 9);
+  printf("%d\n", n);
+  return (0);
+}
