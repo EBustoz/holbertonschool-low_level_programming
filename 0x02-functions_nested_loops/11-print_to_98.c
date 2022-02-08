1d758ed1b6f8780696ee94e4c0070ae6a6417580
Ebustoz <ebustozm@gmail.com>
2022-02-08 19:12:24 +0000

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main : Call a function that prints all natural numbers from n to 98,
+ *        followed by a new line.
+ *
+ *Return: Always 0 (succes)
+ */
+
+int main (void)
+{
+	print_to_98(0);
+  print_to_98(98);
+  print_to_98(111);
+  print_to_98(81);
+  print_to_98(-10);
+  return (0);
+}
