9e195064bd679ad098f3c4122585c5e80ebc0db9
Ebustoz <ebustozm@gmail.com>
2022-02-09 07:10:24 +0000

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabet
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	char alpha;
+	char times;
+
+	alpha = 97;
+	times=0;
+
+	while (times<10)
+	{
+		while (alpha <= 122)
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		alpha=97;
+		times++;
+		_putchar('\n');
+	}
+}
