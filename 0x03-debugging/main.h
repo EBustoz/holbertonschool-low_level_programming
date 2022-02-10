c43f868131b1f49baa63ed3cc4e2727c656c141a
Ebustoz <ebustozm@gmail.com>
2022-02-10 16:59:22 +0000

    2. 0 > 972?

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,21 @@
 
 #include <stdio.h>
 
+/**
+ * positive_or_negative - print if a number is positive or negative
+ * @i: number to evaluate
+ */
+
 void positive_or_negative(int i);
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c);
 
 #endif /* MAIN_H */
