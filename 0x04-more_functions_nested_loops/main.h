4f039bfb7bf9fdbe87a2f9b0a78928e007109f0f
Ebustoz <ebustozm@gmail.com>
2022-02-10 07:10:52 +0000

    0. isupper

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 0 (Always)
+ *
+ */
+
+int _putchar(char c);
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if is UPPERCASE, 0 otherwise
+ */
+
+int _isupper(int c);
+
+#endif
