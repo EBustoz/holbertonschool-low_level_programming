ecb1766c0c4be5a3afd232562f3a2beaf9353c39
Ebustoz <ebustozm@gmail.com>
2022-02-16 03:09:57 +0000

    8. Arrays are not pointers

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -76,4 +76,12 @@ void puts2(char *str);
 
 void puts_half(char *str);
 
+/**
+ * print_array - prints n elements of an array of integers
+ * @a: array
+ * @n: number of elements
+ */
+
+void print_array(int *a, int n);
+
 #endif
