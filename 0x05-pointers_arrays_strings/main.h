2910c0ba2aba223ae06aa952e1001a2b58c0aedf
Ebustoz <ebustozm@gmail.com>
2022-02-16 03:15:10 +0000

    9. strcpy mandatory

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -84,4 +84,15 @@ void puts_half(char *str);
 
 void print_array(int *a, int n);
 
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ * Return: copy of original source
+ */
+
+char *_strcpy(char *dest, char *src);
+
 #endif
