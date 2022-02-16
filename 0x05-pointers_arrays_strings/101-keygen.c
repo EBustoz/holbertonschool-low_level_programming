a75f44cc2fe61577afe7cde29a9ac1e9f75a2973
Ebustoz <ebustozm@gmail.com>
2022-02-16 03:45:32 +0000

    11. Don't hate the hacker, hate the code - REVIEW

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,20 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 /**
- * Program that generate random valid password 
- * Return: Always return 0
+ * main - generates a password
+ *
+ * Return: 0
  */
-
 int main(void)
 {
-	srand((unsigned int)(time(NULL)));
+	char choice, candidates[] =
+		"abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ-.";
+	int i, rand_num, sum = 2772;
 
-	int index = 0;
-	char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/,.-+=~`<>:";
-	for(index = 0; index < 12; index++)
+	srand(time(NULL));
+	for (i = 0; sum > 200; i++)
 	{
-		printf("%c", characters[rand() % (sizeof characters - 1)]);
+		rand_num = rand() % (sizeof(candidates) - 1);
+		choice = candidates[rand_num];
+		putchar(choice);
+		sum -= choice;
 	}
+	if (sum > 122)
+	{
+	    putchar(sum / 2);
+		putchar((sum / 2) + (sum % 2));
+	}
+	else
+		putchar(sum);
+
+	return (0);
 }
