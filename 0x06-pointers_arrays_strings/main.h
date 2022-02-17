6b1d35c32c605e49885e7cd5c1f429a596a942c8
Ebustoz <ebustozm@gmail.com>
2022-02-17 16:04:15 +0000

    4. I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -49,4 +49,12 @@ char *_strncpy(char *dest, char *src, int n);
 
 int _strcmp(char *s1, char *s2);
 
+/**
+ * reverse_array - function that reverses the content of an array of integers
+ * @a: array of integers
+ * @n: number of elements in array
+ */
+
+void reverse_array(int *a, int n);
+
 #endif
