bdc09ddd036d3445605944d02dc4fa7a114e22c6
Ebustoz <ebustozm@gmail.com>
2022-02-21 08:02:11 +0000

    0. memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,24 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 0 (Always)
+ *
+ */
+
+int _putchar(char c);
+
+ /**
+  * _memset - fill n bytes of memory with a constant value
+  * @s: pointer to memory area
+  * @b: constant value
+  * @n: number of bytes to fill
+  * Return: memory area
+  */
+
+ char *_memset(char *s, char b, unsigned int n);
+
+#endif
