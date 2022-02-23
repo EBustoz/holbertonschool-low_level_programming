86e57f5d17ff5f406a3dfc3f982a56ae45c49a19
Ebustoz <ebustozm@gmail.com>
2022-02-23 08:04:17 +0000

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 0 (Always)
+ *
+ */
+
+int _putchar(char c);
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line
+ * @s: string to print
+ * Return: pointer to that character in string (NULL is not located)
+ */
+
+void _puts_recursion(char *s);
+
+#endif
