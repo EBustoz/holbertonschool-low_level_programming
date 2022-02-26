c11eaceb172003a3609f7c2a5597826fe051be6a
Ebustoz <ebustozm@gmail.com>
2022-02-26 05:25:20 +0000

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,23 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 0 (Always)
+ *
+ */
+
+int _putchar(char c);
+
+/**
+ * main - print name of program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[]);
+
+#endif
