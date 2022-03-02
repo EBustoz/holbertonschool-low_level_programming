01b7a5d9b4b514dd9cc3bb267ba1f62f5efa0194
Ebustoz <ebustozm@gmail.com>
2022-03-02 03:07:34 +0000

    4. It's not bragging if you can back it up

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -53,5 +53,14 @@ int _putchar(char c);
   */
 
  int **alloc_grid(int width, int height);
+
+ /**
+  * free_grid - frees a 2 dimensional grid
+  * previously created by your alloc_grid function
+  * @grid: pointer to 2D array
+  * @height: height of array
+  */
+
+ void free_grid(int **grid, int height);
  
 #endif
