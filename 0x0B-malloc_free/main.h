6e53b9d40f4ff7569de1281e8f9a5bdac51e5849
Ebustoz <ebustozm@gmail.com>
2022-03-02 02:38:37 +0000

    0. Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,30 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 0 (Always)
+ *
+ */
+
+int _putchar(char c);
+
+/**
+ * main - print name of program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
+ */
+
+ /**
+  * create_array - create array of chars and initialize with a specific char
+  * @size: size of array
+  * @c: fill array values with this char
+  * Return: pointer to array
+  */
+
+ char *create_array(unsigned int size, char c);
+ 
+#endif
