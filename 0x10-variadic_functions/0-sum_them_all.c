9cf66f03aafdb5481cc20fb7203ec0c4a362f8d3
Ebustoz <ebustozm@gmail.com>
2022-03-11 03:37:03 +0000

    0. Beauty is variable, ugliness is constant - new review

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,29 +1,26 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 
-/** 
- *sum_them_all - Return sum when given unknown amount of args
- *@n: integers to sum
- *Return: sum
+/**
+ * sum_them_all - return sum when given unknown amount of arguments
+ * @n: integers to add
+ * Return: sum
  */
 
- int sum_them_all(const unsigned int n, ...)
- {
-	 va_list valist;
-	 unsigned int sum = 0, i;
-	 
-	 /* Verify if n == 0, return 0 */
-	 if (n == 0)
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int sum = 0, i;
+
+	/* validate valist and initialize */
+	if (n == 0)
 		return (0);
+	va_start(valist, n);
 
-	 /* Initialize the argument list */
-	 va_start(valist, n);
+	/* iterate through list, update sum, free list */
+	for (i = 0; i < n; i++)
+		sum += va_arg(valist, int);
+	va_end(valist);
 
-	 /* iterate through list, upddate sum */
-	 for (i = 0; i < n; i++)
-		 sum += va_arg(valist, int); /* Get the next argument value */
-
-	 va_end(valist); /* Clean up */
-	 
-	 return (sum)
+	return (sum);
 }
